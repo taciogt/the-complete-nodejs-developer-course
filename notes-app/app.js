ef56b41d7298b368d96237c55f05d8d2db164855
Tácio Tavares <taciogt@gmail.com>
2020-11-24 16:12:52 -0300

    section 3, lesson 11

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,6 @@
-const add = require('./utils.js')
-const sum = add(4, -2)
-console.log(sum)
+// const add = require('./utils.js')
+// const sum = add(4, -2)
+// console.log(sum)
 
 // Challenge: Define and use a function in a new file
 //
@@ -9,6 +9,14 @@ console.log(sum)
 // 3. Export getNotes function
 // 4. From app.js, load in and call the function printing message to console
 
-const notes = require('./notes.js')
+// const notes = require('./notes.js')
+//
+// console.log(notes())
 
-console.log(notes())
+const validator = require('validator')
+
+console.log(validator.isEmail('john@email.com'))
+console.log(validator.isEmail('john.not-email'))
+
+console.log(validator.isURL('not.a-url'))
+console.log(validator.isURL('https://this.is-valid.url'))
