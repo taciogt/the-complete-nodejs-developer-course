abf6c536c661521777aff6a925c2353e5d527315
Tácio Tavares <taciogt@gmail.com>
2020-11-25 09:31:53 -0300

    section 4, lesson 16

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,51 @@
 const chalk = require('chalk');
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const command = process.argv[2]
+// Customize yargs version
+yargs.version('1.2.0')
 
-console.log(process.argv)
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!')
+    }
+})
 
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note!')
+    }
+})
+
+// Challenge: Add two new commands
+//
+// 1. Setup command to suppport "list" command (print placeholder message for now)
+// 2. Setup command to suppport "read" command (print placeholder message for now)
+// 3. Test your work by running both commands and ensure correct output
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function () {
+        console.log('Listing all the notes!')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note!')
+    }
+})
+
+// add, remove, read, list
+console.log(yargs.argv)
