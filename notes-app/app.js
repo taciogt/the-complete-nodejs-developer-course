563482e78fa3ba63070c0182cf95171424687bab
Tácio Tavares <taciogt@gmail.com>
2020-11-25 16:31:13 -0300

    section 4, lesson 19

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,17 +1,9 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
 // Customize yargs version
 yargs.version('1.1.0')
-
-// Challenge: Add an option to yargs
-//
-// 1. Setup a body option for the add command
-// 2. Configure a description, make it required, and for it to be a string
-// 3. Log the body value in the handler function
-// 4. Test your work!
-
 
 // Create add command
 yargs.command({
@@ -30,8 +22,7 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
