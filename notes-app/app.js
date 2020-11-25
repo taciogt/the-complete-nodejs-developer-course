ea8ccd4f290203979523ed46461b78e3f066977a
Tácio Tavares <taciogt@gmail.com>
2020-11-25 09:48:25 -0300

    section 4, lesson 17

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,16 +1,37 @@
-const chalk = require('chalk');
+const chalk = require('chalk')
 const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
 // Customize yargs version
-yargs.version('1.2.0')
+yargs.version('1.1.0')
+
+// Challenge: Add an option to yargs
+//
+// 1. Setup a body option for the add command
+// 2. Configure a description, make it required, and for it to be a string
+// 3. Log the body value in the handler function
+// 4. Test your work!
+
 
 // Create add command
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function () {
-        console.log('Adding a new note!')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
     }
 })
 
@@ -22,12 +43,6 @@ yargs.command({
         console.log('Removing the note!')
     }
 })
-
-// Challenge: Add two new commands
-//
-// 1. Setup command to suppport "list" command (print placeholder message for now)
-// 2. Setup command to suppport "read" command (print placeholder message for now)
-// 3. Test your work by running both commands and ensure correct output
 
 // Create list command
 yargs.command({
@@ -47,5 +62,4 @@ yargs.command({
     }
 })
 
-// add, remove, read, list
-console.log(yargs.argv)
+yargs.parse()
