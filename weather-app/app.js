57ca37b1badf7e935d8118b889de1d915240066d
Tácio <taciogt@gmail.com>
2020-12-06 19:37:59 -0300

    section 6, lesson 32

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,16 +1,17 @@
 const request = require('postman-request')
 
-const url = 'http://api.weatherstack.com/current?access_key=2b02f451a08b57fc249556c47753422b&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=2b02f451a08b57fc249556c47753422b&query=37.8267,-122.4233&units=f'
 
 request({
     url: url,
     json: true
 }, (error, response) => {
     // console.log(response.body.current)
+    const description = response.body.current.weather_descriptions[0]
     const temperature = response.body.current.temperature
     const apparent_temperature = response.body.current.feelslike
 
-    console.log("It is currently " + temperature + " degrees out. It feels like " + apparent_temperature + " degrees out")
+    console.log(description + ". It is currently " + temperature + " degrees out. It feels like " + apparent_temperature + " degrees out.")
 })
 
 // Goal: Print a small forecast to the user
