ab7f3233fd3413cb0bfd6f4a307e107032029b23
Tácio <taciogt@gmail.com>
2020-12-08 22:33:44 -0300

    section 6, lesson 35

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,39 +4,45 @@ const fs = require('fs')
 const secrets = JSON.parse(fs.readFileSync('secrets/secrets.json'))
 
 const url = 'http://api.weatherstack.com/current?access_key=' +
-    secrets.weatherStackApiAccessKey + '&query=37.8267,-122.4233&units=f'
+    secrets.weatherStackApiAccessKey + '&query=37.8267,-122.4233'
+// const url = 'http://api.weatherstack.com/current?access_key=' + secrets.weatherStackApiAccessKey + '&query='
 
 request({
     url: url,
     json: true
 }, (error, response) => {
-    // console.log(response.body.current)
-    const description = response.body.current.weather_descriptions[0]
-    const temperature = response.body.current.temperature
-    const apparent_temperature = response.body.current.feelslike
-
-    console.log(description + ". It is currently " + temperature + " degrees out. It feels like " + apparent_temperature + " degrees out.")
+    if (error) {
+        console.log('Unable to connect to weather service!')
+    } else if (response.body.error) {
+        console.log('Unable to find location')
+    } else {
+        const description = response.body.current.weather_descriptions[0]
+        const temperature = response.body.current.temperature
+        const apparent_temperature = response.body.current.feelslike
+        console.log(description + ". It is currently " + temperature + " degrees out. It feels like " + apparent_temperature + " degrees out.")
+    }
 })
 
-// Geocoding
-// Address -> Lat/Long -> Weather
-
-// Goal: Print the lat/long for Los Angeles
+// Goal: Handle errors for geocoding request
 //
-// 1. Fire off a new request to the URL explored in browser
-// 2. Have the request module parse it as JSON
-// 3. Print both the latitude and longitude to the terminal
-// 4. Test your work!
+// 1. Setup an error handler for low-lever errors
+// 2. Test by disabling network request and running the app
+// 3. Setup error handling for no matching results
+// 4. Test by altering the search term and running the app
 
 const mapBoxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=' +
     secrets.mapBoxAccessTolen + '&limit=1'
-console.log(mapBoxUrl)
-request({
-    url: mapBoxUrl,
-    json: true
-}, (error, response) => {
-    console.log(response.body)
-    const lat = response.body.features[0].center[1]
-    const long = response.body.features[0].center[0]
-    console.log('Los Angeles: Lat=' + lat + ', Long=' + long)
-})
+// const mapBoxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/12whenever.json?access_token=' + secrets.mapBoxAccessTolen + '&limit=1'
+request({url: mapBoxUrl, json: true},
+    (error, response) => {
+        if (error) {
+            console.log('Unable to connect to location service!')
+        } else if (response.body.features.length === 0) {
+            console.log('Unable to find a matching location.')
+        } else {
+            const lat = response.body.features[0].center[1]
+            const long = response.body.features[0].center[0]
+            console.log('Los Angeles: Lat=' + lat + ', Long=' + long)
+
+        }
+    })
