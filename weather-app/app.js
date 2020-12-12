4f0b340019bf11afb3baae1beae8718ef6cde33b
Tácio <taciogt@gmail.com>
2020-12-11 23:00:57 -0300

    section 6, lesson 38

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,31 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Belém', (error, data) => {
-    console.log('Error:', error)
-    console.log('Data:', data)
-})
+// Goal: Accept location via command line argument
+//
+// 1. Access the command line argument without yargs
+// 2. Use the string value as the input for geocode
+// 3. Only geocode if a location was provided
+// 4. Test your work with a couple locations
 
-forecast(-23.533,-46.617, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+const locationQuery = process.argv[2]
+if (locationQuery !== undefined) {
+    geocode(locationQuery, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        forecast(data.latitude, data.longitude, (error, forecaseData) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            console.log(data.location)
+            console.log(forecaseData.message)
+        })
+    })
+} else {
+    console.log('No location provided!')
+}
+
+
