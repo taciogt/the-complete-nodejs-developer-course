b954ec1f4c2cfdeedde1a1a4b3aebbd8b78955b1
Tácio <taciogt@gmail.com>
2020-12-13 18:22:58 -0300

    section 7, lesson 44

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,33 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.send([{
+        name: 'Tácio',
+    }, {
+        name: 'Sarah'
+    }])
 })
 
+// Goal: Update routes
+//
+// 1. Setup about route to render a title with HTML
+// 2. Setup a weather route to send back JSON
+//    - Object with forecast and location strings
+// 3. Test your work by visiting both in the browser
+
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.send('<h1>About page</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Your weather')
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Ushuaia'
+    })
 })
 
 app.listen(3000, () => {
