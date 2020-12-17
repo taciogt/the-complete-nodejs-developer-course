2d8662df099fb004ac898cc38038ff27a2d5e5dd
Tácio <taciogt@gmail.com>
2020-12-16 23:08:19 -0300

    section 7, lesson 47

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,14 +3,36 @@ const express = require('express')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
-// Goal: Create two more HTML files
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Tácio'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Weather App',
+        name: 'Tácio'
+    })
+})
+
+// Goal: Creeate a template for help page
 //
-// 1. Create a html page for about with "About" title
-// 2. Create a html page for help with "Help" title
-// 3. Remove the old route handlers for both
-// 4. Visit both in the browser to test your work
+// 1. Setup a help template to render a help message to the screen
+// 2. Setup the help route and render the template with an example message
+// 3. Visit the route in the browser and see your help message print
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help page',
+        message: 'hope this message helps someone'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
